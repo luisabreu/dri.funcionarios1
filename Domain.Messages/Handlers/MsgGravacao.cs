506a16e07686085956d4f19b0e5957ff356ebd13
luisabreu <labreu@gmail.com>
2015-02-04 15:24:50 +0000

    * Added interface for handling commands

---
+++ Domain.Messages/Handlers/MsgGravacao.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Messages.Handlers {
+    public class MsgGravacao {
+        public int Id { get; set; }
+        public int Version { get; set; }
+    }
+}
---
+++ Domain/Handlers/ITrata.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Messages.Comandos;
+using Domain.Messages.Handlers;
+
+namespace Domain.Handlers
+{
+    [ContractClass(typeof(TrataContracto))]
+    public interface ITrata {
+        MsgGravacao Trata(CriaFuncionario comando);
+        MsgGravacao Trata(ModificaContactosFuncionario comando);
+        MsgGravacao Trata(ModificaDadosGeraisFuncionario comando);
+    }
+
+    [ContractClassFor(typeof(ITrata))]
+    abstract class TrataContracto : ITrata {
+        public MsgGravacao Trata(CriaFuncionario comando) {
+            Contract.Requires(comando != null);
+            return default(MsgGravacao);
+        }
+
+        public MsgGravacao Trata(ModificaContactosFuncionario comando) {
+            Contract.Requires(comando != null);
+            return default(MsgGravacao);
+        }
+
+        public MsgGravacao Trata(ModificaDadosGeraisFuncionario comando) {
+            Contract.Requires(comando != null);
+            return default(MsgGravacao);
+        }
+    }
+}
