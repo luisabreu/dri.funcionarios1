576af278468bfd440a6df3f3e65d53745da13f8f
luisabreu <labreu@gmail.com>
2015-02-05 13:39:10 +0000

    * Added test for failure scenario

--- Domain.Tests/Servicos/FuncionalidadesServicoVerificacaoDuplicacaoNif.cs
+++ Domain.Tests/Servicos/FuncionalidadesServicoVerificacaoDuplicacaoNif.cs
@@ -10,7 +10,7 @@ namespace Domain.Tests.Servicos {
 
         [Scenario]
         public void Indica_duplicacao_quando_pesquisa_devolve_true(ServicoVerificacaoDuplicacaoNif servico, int id, string nif, bool resultado) {
-           
+            const int totalRegistosDevolver = 1;
 
             "Dado um servico de verificação"
                 .Given(() => servico = _container.Create<ServicoVerificacaoDuplicacaoNif>());
@@ -22,19 +22,7 @@ namespace Domain.Tests.Servicos {
                 .And(() => nif = "123456789");
 
             "E alguns mocks"
-                .And(() => {
-                    var query = _container.GetMock<ISQLQuery>();
-                    query.Setup(q => q.SetString(It.IsAny<string>(), nif))
-                        .Returns(query.Object);
-                    query.Setup(q => q.SetInt32(It.IsAny<string>(), id))
-                        .Returns(query.Object);
-                    query.Setup(q => q.UniqueResult<int>())
-                        .Returns(1);
-                    _container.GetMock<ISession>()
-                        .Setup(s => s.CreateSQLQuery(It.IsAny<string>()))
-                        .Returns(query.Object);
-
-                     });
+                .And(() => PreparaMocks(id, nif, totalRegistosDevolver));
 
             "Quando verificamos a duplicação"
                 .When(() => resultado = servico.NifDuplicado(nif, id));
@@ -47,6 +35,49 @@ namespace Domain.Tests.Servicos {
                          _container.GetMock<ISession>().VerifyAll();
                          _container.GetMock<ISQLQuery>().VerifyAll();
                      });
+        }
+        
+        
+        [Scenario]
+        public void Sem_duplicacao_quando_pesquisa_devolve_false(ServicoVerificacaoDuplicacaoNif servico, int id, string nif, bool resultado) {
+            const int totalRegistosDevolver = 0;
+
+            "Dado um servico de verificação"
+                .Given(() => servico = _container.Create<ServicoVerificacaoDuplicacaoNif>());
+
+            "e um id"
+                .And(() => id = 1);
+
+            "e um nif"
+                .And(() => nif = "123456789");
+
+            "E alguns mocks"
+                .And(() => PreparaMocks(id, nif, totalRegistosDevolver));
+
+            "Quando verificamos a duplicação"
+                .When(() => resultado = servico.NifDuplicado(nif, id));
+
+            "O resultado deve ser false"
+                .Then(() => resultado.Should().BeFalse());
+
+            "E servico deve ter sido usado corretamente"
+                .And(() => {
+                         _container.GetMock<ISession>().VerifyAll();
+                         _container.GetMock<ISQLQuery>().VerifyAll();
+                     });
+        }
+
+        private void PreparaMocks(int id, string nif, int totalADevolver) {
+            var query = _container.GetMock<ISQLQuery>();
+            query.Setup(q => q.SetString(It.IsAny<string>(), nif))
+                .Returns(query.Object);
+            query.Setup(q => q.SetInt32(It.IsAny<string>(), id))
+                .Returns(query.Object);
+            query.Setup(q => q.UniqueResult<int>())
+                .Returns(totalADevolver);
+            _container.GetMock<ISession>()
+                .Setup(s => s.CreateSQLQuery(It.IsAny<string>()))
+                .Returns(query.Object);
         }
     }
 }
