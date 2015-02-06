a84d09b69900ed8f0d0e4d307e11f57e6029c03b
luisabreu <labreu@gmail.com>
2015-02-06 09:53:04 +0000

    * Added GestorTransacoes for connecting to local db

---
+++ site/Helpers/GestorTransacoes.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+using GestorTransacoesBase = Domain.Mapeamentos.GestorTransacoes;
+
+namespace site.Helpers {
+    public class GestorTransacoes : GestorTransacoesBase {
+        public override string ObtemCnnString() {
+            return ConfigurationManager.ConnectionStrings["bd"].ConnectionString;
+        }
+    }
+}
