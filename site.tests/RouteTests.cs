89b784377b6fc46da23b321d63ae5a29a5034379
luisabreu <labreu@gmail.com>
2015-02-06 10:18:37 +0000

    * Added nuget packages to mvc test project

---
+++ site.tests/RouteTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace site.tests
+{
+    public class RouteTests {
+        public void Verifica_regras_rotas() {
+            
+        }
+    }
+}
