519a66d380d1299f160cd76b222de32dbbda13e1
luisabreu <labreu@gmail.com>
2015-02-09 14:57:48 +0000

    * Added missing unit tests for controller actions' * Fixed project properties so that contracts are generated everywhere

--- site.tests/Controllers/FuncionalidadesCriacaoModificacaoFuncionarios.cs
+++ site.tests/Controllers/FuncionalidadesCriacaoModificacaoFuncionarios.cs
@@ -288,6 +288,63 @@ namespace site.tests.Controllers {
                          modelo.TiposFuncionario.Should().BeEquivalentTo(tipos);
                      });
         }
+        
+        [Scenario]
+        public void Adiciona_contacto_sem_sucesso(FuncionariosController controlador, int id, int versao, string contacto, ActionResult resultado) {
+            var funcionarioDto = new FuncionarioDto();
+            var tipos = new List<TipoFuncionario> {new TipoFuncionario(1, "teste")};
+
+            "Quando recebemos um controlador"
+                .Given(() => controlador = _container.Create<FuncionariosController>());
+
+            "E um id e versão inicializados"
+                .And(() => id = versao = 1);
+
+            "E um contacto inválido"
+                .And(() => contacto = "222");
+
+            "E um conjunto de mocks"
+                .And(() => {
+                         var query = _container.GetMock<IQueryOver<TipoFuncionario, TipoFuncionario>>();
+                         var tran = _container.GetMock<ITransaction>();
+                         _container.GetMock<ISession>()
+                             .Setup(s => s.BeginTransaction())
+                             .Returns(tran.Object);
+                         tran.Setup(t => t.Commit());
+                         _container.GetMock<ISession>()
+                             .Setup(s => s.QueryOver<TipoFuncionario>())
+                             .Returns(query.Object);
+                         query.Setup(q => q.List<TipoFuncionario>())
+                             .Returns(tipos);
+
+                         _container.GetMock<IProcessador>()
+                             .Setup(p => p.Trata(It.IsAny<ModificaContactosFuncionario>()))
+                             .Returns(new MsgGravacao {Id = 1, Versao = 2});
+
+                         _container.GetMock<ISession>()
+                             .Setup(s => s.Load<FuncionarioDto>(1))
+                             .Returns(funcionarioDto);
+                         _container.GetMock<ITransaction>()
+                             .Setup(t => t.Dispose());
+                     });
+
+            "Quando tentamos adicionar o contacto"
+                .When(() => resultado = controlador.AdicionaContacto(id, versao, contacto));
+
+            "Então devemos ser redirecionados para vista funcionários"
+                .Then(() => ((ViewResult) resultado).ViewName.ToLower().Should().Be("funcionario"));
+
+            "E devemos obter os valores corretos no modelo"
+                .And(() => {
+                         var modelo = (DadosFormularioFuncionario) ((ViewResult) resultado).Model;
+                         modelo.Funcionario.Should().BeSameAs(funcionarioDto);
+                         modelo.Novo.Should().BeFalse();
+                         modelo.TiposFuncionario.Should().BeEquivalentTo(tipos);
+                     });
+
+            "E ainda devemos ter o erro"
+                .And(() => controlador.ModelState.Should().ContainKey("total"));
+        }
 
         [Scenario]
         public void Remove_contacto_com_sucesso(FuncionariosController controlador, int id, int versao, string contacto, ActionResult resultado) {
@@ -329,7 +386,7 @@ namespace site.tests.Controllers {
                      });
 
             "Quando tentamos remover o contacto"
-                .When(() => resultado = controlador.AdicionaContacto(id, versao, contacto));
+                .When(() => resultado = controlador.EliminaContacto(id, versao, contacto));
 
             "Então devemos ser redirecionados para vista funcionários"
                 .Then(() => ((ViewResult) resultado).ViewName.ToLower().Should().Be("funcionario"));
@@ -341,6 +398,63 @@ namespace site.tests.Controllers {
                          modelo.Novo.Should().BeFalse();
                          modelo.TiposFuncionario.Should().BeEquivalentTo(tipos);
                      });
+        }
+        
+        [Scenario]
+        public void Remove_contacto_sem_sucesso(FuncionariosController controlador, int id, int versao, string contacto, ActionResult resultado) {
+            var funcionarioDto = new FuncionarioDto();
+            var tipos = new List<TipoFuncionario> {new TipoFuncionario(1, "teste")};
+
+            "Quando recebemos um controlador"
+                .Given(() => controlador = _container.Create<FuncionariosController>());
+
+            "E um id e versão inicializados"
+                .And(() => id = versao = 1);
+
+            "E um contacto inválido"
+                .And(() => contacto = "333");
+
+            "E um conjunto de mocks"
+                .And(() => {
+                         var query = _container.GetMock<IQueryOver<TipoFuncionario, TipoFuncionario>>();
+                         var tran = _container.GetMock<ITransaction>();
+                         _container.GetMock<ISession>()
+                             .Setup(s => s.BeginTransaction())
+                             .Returns(tran.Object);
+                         tran.Setup(t => t.Commit());
+                         _container.GetMock<ISession>()
+                             .Setup(s => s.QueryOver<TipoFuncionario>())
+                             .Returns(query.Object);
+                         query.Setup(q => q.List<TipoFuncionario>())
+                             .Returns(tipos);
+
+                         _container.GetMock<IProcessador>()
+                             .Setup(p => p.Trata(It.IsAny<ModificaContactosFuncionario>()))
+                             .Returns(new MsgGravacao {Id = 1, Versao = 2});
+
+                         _container.GetMock<ISession>()
+                             .Setup(s => s.Load<FuncionarioDto>(1))
+                             .Returns(funcionarioDto);
+                         _container.GetMock<ITransaction>()
+                             .Setup(t => t.Dispose());
+                     });
+
+            "Quando tentamos remover o contacto"
+                .When(() => resultado = controlador.EliminaContacto(id, versao, contacto));
+
+            "Então devemos ser redirecionados para vista funcionários"
+                .Then(() => ((ViewResult) resultado).ViewName.ToLower().Should().Be("funcionario"));
+
+            "E devemos obter os valores corretos no modelo"
+                .And(() => {
+                         var modelo = (DadosFormularioFuncionario) ((ViewResult) resultado).Model;
+                         modelo.Funcionario.Should().BeSameAs(funcionarioDto);
+                         modelo.Novo.Should().BeFalse();
+                         modelo.TiposFuncionario.Should().BeEquivalentTo(tipos);
+                     });
+
+            "E ainda devemos ter o erro no model state"
+                .And(() => controlador.ModelState.Should().ContainKey("total"));
         }
     }
 }
